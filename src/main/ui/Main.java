7f32061632239606341aa2aa28db3c3b5c8f3203
benanthony <benantho@github.students.cs.ubc.ca>
2020-05-10 20:41:11 -0700

    cleaned up deprecated functionality

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -10,20 +10,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-/*//        Question q0 = new Question(0, "I am interested in abstract ideas", false);
-//        Question q1 = new Question(1, "I am always on time", false);
-//        Question q2 = new Question(2, "I am not the life of the party", true);
-//        Question q3 = new Question(3, "I like to please others", false);
-//        Question q4 = new Question(4, "I am fearful of the future", false);
-//        ArrayList<Question> bigFiveQuestions = new ArrayList<>(Arrays.asList(q0, q1, q2, q3, q4));
-        */
-
-//        WebScraper webScraper = new WebScraper();
-//        ArrayList<Question> bigFiveQuestions = webScraper.createQuestionBank(10);
-//        Quiz bigFiveQuiz = new Quiz(bigFiveQuestions);
-//        QuizRunner quizRunner = new QuizRunner();
-//        quizRunner.run(bigFiveQuiz);
-
         StaticGUI.run();
     }
 }
